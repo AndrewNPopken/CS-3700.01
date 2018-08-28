a26a59d2ca59c240d4ec01dd5e18f94cc1300c8e
Andrew Popken <kradysuom@hotmail.com>
2018-08-28 12:29:57 -0700

    Added Hello World java file

---
+++ CS3700HW1.java
@@ -0,0 +1,17 @@
+
+package cs3700hw1;
+
+/**
+ *
+ * @author ANP Offline
+ */
+public class CS3700HW1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+    
+}
