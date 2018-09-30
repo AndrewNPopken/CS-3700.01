00fd4e2e57042eb8f2836f9c7990f07341d4542a
AndrewNPopken <kradysuom@hotmail.com>
2018-09-29 18:37:41 -0700

    Added Lecture example programs
    included a RunAll.bat file that runs the programs back-to-back (assuming you can run .bat) in a Command Prompt

---
+++ CS3700HW2Lec/RunAll.bat
@@ -0,0 +1,65 @@
+echo off
+cls
+echo.
+echo HelloRunnable: 
+java -cp .\Lec3\ HelloRunnable
+echo.
+echo HelloThread: 
+java -cp .\Lec3\ HelloThread
+echo.
+echo SimpleThreads: 
+java -cp .\Lec3\ SimpleThreads
+echo.
+echo SleepMessages: 
+java -cp .\Lec3\ SleepMessages
+echo.
+echo ProducerConsumerExample: 
+java -cp .\Lec5\ ProducerConsumerExample.ProducerConsumerExample
+echo.
+echo ForkBlur: 
+java -cp .\Lec6\ ForkBlur
+echo.
+echo AtomicInteger: 
+java -cp .\AtomicInteger\ TestThread
+echo.
+echo AtomicIntegerArray: 
+java -cp .\AtomicIntegerArray\ TestThread
+echo.
+echo AtomicReference: 
+java -cp .\AtomicReference\ TestThread
+echo.
+echo AtomicReferenceArray: 
+java -cp .\AtomicReferenceArray\ TestThread
+echo.
+echo Barrier: 
+java -cp .\Barrier\ Lect4
+echo.
+echo BlockingQueue: 
+java -cp .\BlockingQueue\ TestThread
+echo.
+echo ConcurrentMap: 
+java -cp .\ConcurrentMap\ TestThread
+echo.
+echo ConcurrentNavigableMap: 
+java -cp .\ConcurrentNavigableMap\ TestThread
+echo.
+echo Executor-execute: 
+java -cp .\Executor-execute\ TestThread
+echo.
+echo ExecutorService-schedule: 
+java -cp .\ExecutorService-schedule\ TestThread
+echo.
+echo ExecutorService-submit: 
+java -cp .\ExecutorService-submit\ TestThread
+echo.
+echo Future: 
+java -cp .\Future\ TestThread
+echo.
+echo Memoization: 
+java -cp .\Memoization\ Lect4
+echo.
+echo.
+echo Deadlock: 
+java -cp .\Lec4\ Deadlock
+echo.
+pause
